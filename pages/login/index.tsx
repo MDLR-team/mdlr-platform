7d2a09f07003e3cc5299ac3e2ac16fdfd0bad75a
ilia-kuzmin-xyz <markkabierski@gmail.com>
2024-08-26 18:18:44 +0100

    Enhanced login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,6 +6,7 @@ import {
   InputLabel,
   Input,
   FormHelperText,
+  Box,
 } from "@mui/material";
 
 import { createClient } from "@/utils/supabase/component";
@@ -33,35 +34,83 @@ const LoginPage = () => {
   return (
     <Wrapper>
       <AuthWrapper>
+        <Box
+          sx={{
+            fontSize: "24px",
+            fontWeight: "bold",
+            marginBottom: "24px",
+            color: "black",
+          }}
+        >
+          MDLR
+        </Box>
+
         <main>
           <form>
-            <FormControl>
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormControl>
-            <FormControl>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormControl>
-            <Button variant="contained" onClick={handleFormSubmit}>
-              {isLogin ? "Log in" : "Sign up"}
-            </Button>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "16px",
+                width: "100%",
+                minWidth: "300px",
+                maxWidth: "350px",
+              }}
+            >
+              <FormControl>
+                <Box>Email</Box>
+                <Input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <Box>Password</Box>
+                <Input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+
+              <Box
+                sx={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "24px",
+                  marginTop: "48px",
+                }}
+              >
+                <Box>
+                  By clicking the Log in button, you agree to MDLR{" "}
+                  <u>Terms of Service</u> and <u>Privacy Policy</u>.
+                </Box>
+
+                <Button
+                  size="large"
+                  sx={{
+                    height: "56px",
+                    backgroundColor: "black !important",
+                    borderRadius: "30px !important",
+                    color: "white !important",
+                  }}
+                  variant="contained"
+                  onClick={handleFormSubmit}
+                >
+                  {isLogin ? "Log in" : "Sign up"}
+                </Button>
+              </Box>
+            </Box>
           </form>
         </main>
 
-        <Button variant="text" onClick={() => setIsLogin(!isLogin)}>
+        {/* <Button variant="text" onClick={() => setIsLogin(!isLogin)}>
           {isLogin ? "Switch to Sign up" : "Switch to Log in"}
-        </Button>
+        </Button> */}
       </AuthWrapper>
     </Wrapper>
   );
@@ -72,8 +121,32 @@ const Wrapper = styled.div`
   height: 100vh;
 
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  & input {
+    padding: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+
+    height: 56px;
+  }
+
+  &,
+  & * {
+    font-size: 14px;
+  }
+
+  & .MuiInputBase-root {
+    padding: 0px;
+  }
+
+  & .MuiFormControl-root {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
 `;
 
 const AuthWrapper = styled.div`
@@ -85,7 +158,6 @@ const AuthWrapper = styled.div`
   row-gap: 36px;
 
   padding: 30px;
-  border: 1px solid grey;
 `;
 
 export default LoginPage;
